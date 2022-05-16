ccae6916be12ebab8ccf6d1cef8bb1c886bf8b8f
vn012 <104809895+vn012@users.noreply.github.com>
2022-05-16 20:18:45 -0300

    arquivo python e arquivo txt

---
+++ Grafos.py
@@ -0,0 +1,113 @@
+import numpy as np
+
+texto = open('Grafo.txt')  # Lê um arquivo txt
+grafo = texto.readlines()  # readLijnes tranforma cada linha em uma lista
+linhasGrafo = len(grafo)  # retorna o indice maximo
+# print(grafo)
+# print(type(grafo))
+
+
+
+#  A. Numero de vertices
+def n_vertice():
+    numeroVertice = (grafo[0])
+    return numeroVertice
+
+
+print("O numero de vertives é: ", n_vertice())
+
+#  B. Numero de arestas
+def n_arestas() :
+    return len(grafo) - 1
+
+print("O número de arestas é: ", n_arestas())
+
+
+#  C. grau Maximo
+''''
+def grau_max():
+    i = 1
+    j = 0
+    n1 = 0
+    n2 = 0
+    n3 = 0
+    n4 = 0
+    grauVet = [4]
+    grauMax = int
+
+    for i in range(n_arestas()):  # definindo o vetor
+        grauVet.append(0)
+
+
+    for i  in range(5):
+        for j in range (2):
+            if j > 2:
+                j = 0
+                if grafo[1][0] == grafo[i][j]:
+                    n1 = n1 + 1
+                    grauVet.pop(0)
+                    grauVet.insert(0, n1)
+            else:
+                if grafo[1][0] == grafo[i][j]:
+                    n1 = n1 + 1
+                    grauVet.pop(0)
+                    grauVet.insert(0, n1)
+
+            j = j + 2
+        i = i + 1
+
+
+    return(max(grauVet)) , grauVet
+
+
+print("Grau Maximo: ", grau_max())
+# comprar o 1 de 1 2 com todo o resto, e fazer isso com tudo
+
+
+
+
+
+'''
+
+#  D. grau Maximo
+
+#  E. Representação
+n = 0
+i = 0
+j = 0
+compVet = []
+grafoTrans = [[0, 0], [0, 0], [0, 0], [0, 0]]
+
+matriz = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+
+for l in range(1, 5):
+    for g in range(1,5):
+        grafoTrans.insert(l-1, int(grafo[l][0]))
+
+for i in range(0, 4):
+
+    for j in range(0, 4):
+
+        compVet =[i+1, j+1]
+        if int (grafoTrans[0]):
+            matriz[i][j] = 1
+
+
+print("=====", grafoTrans)
+print(grafo[2][0], grafo[2][2])
+# print(compVet)
+# print(type(grafo[0]))
+print(matriz)
+print(grafo[1])
+
+
+
+
+
+
+#  F. Informar a saída para busca em largura ou em profundidade
+
+#  G. Informar se existem componentes conexas
+
+
+
